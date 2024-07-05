04efdc3d29e91150c4bcd2b466d0a0a73823c61c
ikethedev <isaacdpresley@gmail.com>
2024-07-05 11:08:45 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import Business from "./components/Business.jsx"
+
+function App() {
+
+  return (
+    <>
+       <Business />
+    </>
+  )
+}
+
+export default App
